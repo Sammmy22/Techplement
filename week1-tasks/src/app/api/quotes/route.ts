d3e467c6e156e5981e51dcd34b703191afd8179f
Sambhav Choudhary <sambhavc225@outlook.com>
2024-08-01 22:45:54 +0530

    Made API route to get a random quote

---
+++ week1-tasks/src/app/api/quotes/route.ts
@@ -0,0 +1,20 @@
+// /app/api/hello/route.js
+
+import QUOTES from "./quotes.json";
+
+export async function GET(request: Request) {
+  const randomIndex = Math.floor(Math.random() * QUOTES.length);
+  const randomQuote = QUOTES[randomIndex];
+  return new Response(JSON.stringify({ randomQuote }), {
+    status: 200,
+    headers: { "Content-Type": "application/json" },
+  });
+}
+
+export async function POST(request: Request) {
+  const data = await request.json();
+  return new Response(JSON.stringify({ received: data }), {
+    status: 201,
+    headers: { "Content-Type": "application/json" },
+  });
+}
